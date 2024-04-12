7b1cf8b85fe4493e8d120308128e9cd2338c838e
trumk <nguyentrungtest2292003@gmail.com>
2024-04-12 12:44:29 +0700

    Update index.js

--- frontend/src/pages/faculty/[id]/index.js
+++ frontend/src/pages/faculty/[id]/index.js
@@ -7,16 +7,18 @@ import {
     Card,
     CardHeader,
     CardBody,
+    Avatar
 } from '@material-tailwind/react'
 import { useSelector } from 'react-redux'
 import { getAllEventsByFaculty } from '../../../redux/apiRequest'
 import { useDispatch } from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { Preview } from '../../../components/manage/preview'
 import { format } from 'date-fns'
 import { ArrowRight } from 'lucide-react';
 
 export const FacultyMainPage = () => {
-    const { facultyId } = useParams()
+    const { id } = useParams()
     const user = useSelector((state) => state.auth.login.currentUser);
 
     const eventInFaculty = useSelector((state) => state.faculty.getEventsByFaculty?.filterEvent)
@@ -24,7 +26,7 @@ export const FacultyMainPage = () => {
 
     useEffect(() => {
         if (user) {
-            dispatch(getAllEventsByFaculty(facultyId, user.accessToken))
+            dispatch(getAllEventsByFaculty(id, user.accessToken))
         }
     }, [user, dispatch])
     const eventData = eventInFaculty?.events
@@ -37,7 +39,7 @@ export const FacultyMainPage = () => {
                 <div className="ml-5 w-full h-full">
                     <div className='mt-10 flex items-start gap-5 flex-wrap'>
                         {eventData?.map(event => (
-                            <div key={event._id} className="m-2 flex-none relative" style={{ width: '26rem' }}>
+                            <div key={event._id} className="m-2 flex-none relative" style={{width: '26rem' }}>
                                 <div className="absolute w-full h-full bg-teal-900 rounded-xl" />
                                 <Card color="teal" shadow={false} className="w-full cursor-pointer transform transition-transform duration-200 hover:translate-x-3 hover:-translate-y-3 z-10">
                                     <CardHeader
@@ -58,7 +60,7 @@ export const FacultyMainPage = () => {
                                         </div>
                                     </CardHeader>
                                     <CardBody className="mb-6 ml-4 p-0">
-                                        <Link to={`/faculty/${facultyId}/event/${event._id}`}><Button variant='outlined' className='flex items-center gap-2'>Explore more about the event <ArrowRight className='w-6' /> </Button> </Link>
+                                        <Link to={`/faculty/${id}/event/${event._id}`}><Button variant='outlined' className='flex items-center gap-2'>Explore more about the event <ArrowRight className='w-6' /> </Button> </Link>
                                     </CardBody>
                                 </Card>
                             </div>
