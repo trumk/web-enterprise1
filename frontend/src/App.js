9301b5f6481b84edd74ccc5aad8a28b4febeccc8
trumk <110820114+trumk@users.noreply.github.com>
2024-04-12 12:37:38 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,14 +21,17 @@ import { ContributionDetail } from "./pages/admin/contribution/[id]/detail";
 import { User } from "./pages/admin/user";
 import { UserDetail } from "./pages/admin/user/[id]/detail";
 import { EditUser } from "./pages/admin/user/[id]/edit";
-import { EnrollFaculty } from "./pages/faculty/[facultyId]/enroll";
-import { FacultyMainPage } from "./pages/faculty/[facultyId]";
-import { EventInfo } from "./pages/faculty/[facultyId]/event/[eventId]";
-import { SubmitContribution } from "./pages/faculty/[facultyId]/event/[eventId]/contribution/submit";
-import { ReadContribution } from "./pages/faculty/[facultyId]/event/[eventId]/contribution/[contributionId]/read";
+import { EnrollFaculty } from "./pages/faculty/[id]/enroll";
+import { FacultyMainPage } from "./pages/faculty/[id]";
+import { EventInfo } from "./pages/faculty/[id]/event/[eventId]";
+import { SubmitContribution } from "./pages/faculty/[id]/event/[eventId]/contribution/submit";
+import { ReadContribution } from "./pages/faculty/[id]/event/[eventId]/contribution/[contributionId]/read";
 import { MarketingCoordinatorPage } from "./pages/marketingCoordinator";
-import { MyContributionPage } from "./pages/userContribution";
-import { EditContribution } from "./pages/userContribution/[id]/edit";
+import { MarketingManagerPage } from "./pages/marketingManager";
+import { FacultyManager } from "./pages/marketingManager/faculty";
+import { FacultyDetailManager } from "./pages/marketingManager/[id]/detailFaculty";
+import { EventDetailManager } from "./pages/marketingManager/[id]/eventDetail";
+import { ContributionDetailManager } from "./pages/marketingManager/[id]/contributionDetail";
 
 function App() {
   return (
@@ -39,23 +42,27 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/terms" element={<Term />} />
-          <Route path="/faculty/:facultyId/enroll" element={<EnrollFaculty />} />
-          <Route path="/faculty/:facultyId" element={<FacultyMainPage />} />
-          <Route path="/faculty/:facultyId/event/:eventId" element={<EventInfo />} />
+          <Route path="/faculty/:id/enroll" element={<EnrollFaculty />} />
+          <Route path="/faculty/:id" element={<FacultyMainPage />} />
+          <Route path="/faculty/:id/event/:id" element={<EventInfo />} />
           <Route
-            path="/faculty/:facultyId/event/:eventId/contribution/submit"
+            path="/faculty/:id/event/:id/contribution/submit"
             element={<SubmitContribution />}
           />
           <Route
-            path="/faculty/:facultyId/event/:eventId/contribution/:contributionId/read"
+            path="/faculty/:id/event/:id/contribution/:contributionId/read"
             element={<ReadContribution />}
           />
           {/* User Route */}
           <Route path="/user/:id/profile" element={<UserProfile />} />
           <Route path="/user/:id/changePassword" element={<ChangePassword />} />
           <Route path="/user/:id/edit" element={<EditProfile />} />
-          <Route path="/userContribution" element={<MyContributionPage />} />
-          <Route path="/userContribution/:id/edit" element={<EditContribution />} />
+          {/* Marketing Manager */}
+          <Route path="/marketingManager" element={<MarketingManagerPage/>}/>
+          <Route path="/marketingManager/faculty" element={<FacultyManager/>}/>
+          <Route path="/marketingManager/faculty/:id" element={<FacultyDetailManager/>}/>
+          <Route path="/marketingManager/event/:id" element={<EventDetailManager/>}/>
+          <Route path="/marketingManager/contribution/:id" element={<ContributionDetailManager/>}/>
           {/* Marketing Coordinator */}
           <Route path="/marketingCoordinator" element={<MarketingCoordinatorPage/>}/>
           {/* Admin Route */}
