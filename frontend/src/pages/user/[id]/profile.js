bc5f111ca26029f726ce55caa5cd69be4d453215
bathanh321 <thanhnbgch211319@fpt.edu.vn>
2024-04-15 10:51:07 +0700

    fix toast

--- frontend/src/pages/user/[id]/profile.js
+++ frontend/src/pages/user/[id]/profile.js
@@ -19,20 +19,20 @@ export const UserProfile = () => {
       dispatch(getSelf(user?._id));
     }
   }, [dispatch, user]);
-  // useEffect(() => {
-  //   if (user && user?.accessToken) {
-  //     dispatch(getOneFaculty(facultyId, user.accessToken));
-  //   }
-  // }, [dispatch, faculty, user]);
+  useEffect(() => {
+    if (user && user?.accessToken) {
+      dispatch(getOneFaculty(facultyId, user.accessToken));
+    }
+  }, [dispatch, facultyId, user]);
 
-  const profile = useSelector((state) => state.user.user.user);
+  const profile = useSelector((state) => state.user.user?.user);
   return (
     <>
       <NavbarDefault />
       <div className='flex gap-6'>
         <DefaultSidebar />
         <div className='w-full'>
-          <div class="border h-100 rounded-md mt-5 dark:!bg-navy-800 shadow-shadow-500 shadow-3xl rounded-primary relative mx-auto flex w-full max-w-[550px] flex-col items-center bg-white bg-cover bg-clip-border p-[16px] dark:text-white dark:shadow-none">
+          <div class="border h-100 border-gray-900 rounded-md mt-5 dark:!bg-navy-800 shadow-shadow-500 shadow-3xl rounded-primary relative mx-auto flex w-full max-w-[550px] flex-col items-center bg-white bg-cover bg-clip-border p-[16px] dark:text-white dark:shadow-none">
             <div class="relative mt-1 flex h-32 w-full justify-center rounded-xl bg-cover">
               <img class="h-full w-full rounded-xl" src="https://i.ibb.co/FWggPq1/banner.png" />
               <div class="absolute -bottom-12 flex h-[88px] w-[88px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400">
@@ -47,10 +47,7 @@ export const UserProfile = () => {
                     </button>
                   </Link>
                 </Tooltip>
-
-
               </div>
-
             </div>
             <div class="mt-16 flex flex-col items-center">
               <h4 class="text-bluePrimary text-xl font-bold">{profile?.firstName} {profile.lastName} </h4>
@@ -64,6 +61,10 @@ export const UserProfile = () => {
               <div class="flex flex-col items-center justify-center">
                 <h3 class="text-bluePrimary text-2xl font-bold">Faculty</h3>
                 <p class="text-lightSecondary text-sm font-normal">{faculty?.Faculty.facultyName}</p>
+              </div>
+              <div class="flex flex-col items-center justify-center">
+                <h3 class="text-bluePrimary text-2xl font-bold">Birthday</h3>
+                <p class="text-lightSecondary text-sm font-normal">{profile?.birthDay ? format(new Date(profile?.birthDay), 'MMMM dd,yyyy') : 'N/A'}</p>
               </div>
             </div>
           </div>
