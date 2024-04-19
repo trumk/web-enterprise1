37ce15e8ad05d4d97f54f8d7f9eaa8804fd2fe85
dufefai <130550324+dufefai@users.noreply.github.com>
2024-04-19 21:09:02 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -51,10 +51,10 @@ app.use("/contribution", contributionRouter);
 
 app.user(express.static(path.join(__dirname,"")))
 
-app.use(express.static(path.join(__dirname, '../../../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
 });
 
 const hostname = 'localhost'
