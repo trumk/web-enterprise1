28c17342d7385229540605d020b6ae1a9481649f
dufefai <130550324+dufefai@users.noreply.github.com>
2024-04-23 22:54:46 +0700

    Update Login.js

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -30,7 +30,7 @@ export default function Login() {
             navigate("/marketingManager")
         } else if (currentUser.role === "guest") {
             navigate("/guest")
-        } else if(!currentUser){
+        } else {
             navigate("/dashboard")
         }
     }
