d5809cf7b4c2722a61b1d3d034a7a787e733261e
dufefai <130550324+dufefai@users.noreply.github.com>
2024-04-25 00:01:38 +0700

    Update apiRequest.js

--- frontend/src/redux/apiRequest.js
+++ frontend/src/redux/apiRequest.js
@@ -138,10 +138,10 @@ import {
 } from "./contributionSlice";
 import { toast } from "react-toastify";
 
-//const BACKEND_URL = "https://web-enterprise1.onrender.com";
+const BACKEND_URL = "https://web-enterprise1.onrender.com";
 
-//const BACKEND_URL = "https://web-enterprise1-xd0w.onrender.com";
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://web-enterprise1-xd0w.onrender.com";
+//const BACKEND_URL = "http://localhost:5000";
 
 //auth
 export const loginUser = async (user, dispatch) => {
